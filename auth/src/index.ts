19bd61e921c55f6a0f88277620f7a2a7d1bba1de
Stephen Isla <sisla@deloitte.com.au>
2021-08-27 14:27:25 +1000

    added dev1

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -3,7 +3,7 @@ import mongoose from 'mongoose';
 import { app } from './app';
 
 const start = async () => {
-  console.log("Starting up, let us try again...");
+  console.log("Starting up, let us try again... last time...");
   if (!process.env.JWT_KEY) {
     throw new Error('JWT_KEY must be defined');
   }
