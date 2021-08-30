3653b63c9dfa38b8ab9a3f08a2185f0a1c35944b
Stephen Isla <sisla@deloitte.com.au>
2021-08-30 12:14:14 +1000

    updated as per 513 instruction

--- client/api/build-client.js
+++ client/api/build-client.js
@@ -6,7 +6,7 @@ export default ({ req }) => {
 
     return axios.create({
       baseURL:
-        'http://ingress-nginx-controller.ingress-nginx.svc.cluster.local',
+        'http://www.sisla-ticketing.xyz',
       headers: req.headers,
     });
   } else {
